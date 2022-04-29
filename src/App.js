dfd0f50b8e6f932408816a661e388ba7b266238c
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-04-29 20:20:16 +0430

    refactor get products and create firebase utils

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import { SetProductsAction } from './redux/products/setProducts';
-import './App.scss';
 import CheckoutPage from './pages/checkhout/checkout';
-import { db } from './firebase/firebaseConfig.js';
-import { collection, getDocs } from 'firebase/firestore';
 import Wrapper from './components/wrapper/wrapper.component';
 import Navigations from './components/navigations/navigations.component';
+
+import './App.scss';
+import { getDocsFromCollection } from './firebase/firebase.utils';
+
 const App = () => {
   const [loading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getProducts = async () => {
-      const productsRef = collection(db, 'products');
-      const data = await getDocs(productsRef);
-      dispatch(SetProductsAction(data.docs.map((doc) => ({ ...doc.data() }))));
+      const data = await getDocsFromCollection('products');
+      dispatch(SetProductsAction(data));
       setIsLoading(false);
     };
     getProducts();
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,14 +1,15 @@
-import firebase from "firebase/compat/app";
-import { getFirestore } from "firebase/firestore";
-import "firebase/compat/firestore";
+import firebase from 'firebase/compat/app';
+import { getFirestore } from 'firebase/firestore';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCOFJLP2RD0jweFa_VNar2YQHkYsvUtyw4",
-  authDomain: "game-vision-66003.firebaseapp.com",
-  projectId: "game-vision-66003",
-  storageBucket: "game-vision-66003.appspot.com",
-  messagingSenderId: "544353822649",
-  appId: "1:544353822649:web:9269ff8a19300256793868",
-  measurementId: "G-QJHWNLFHPG",
+  apiKey: 'AIzaSyCOFJLP2RD0jweFa_VNar2YQHkYsvUtyw4',
+  authDomain: 'game-vision-66003.firebaseapp.com',
+  projectId: 'game-vision-66003',
+  storageBucket: 'game-vision-66003.appspot.com',
+  messagingSenderId: '544353822649',
+  appId: '1:544353822649:web:9269ff8a19300256793868',
+  measurementId: 'G-QJHWNLFHPG',
 };
 
 const app = firebase.initializeApp(firebaseConfig);
