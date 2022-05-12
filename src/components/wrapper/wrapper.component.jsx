9b6b3e7f45658326e6377ea0f23036420aaf8d1b
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-12 15:02:05 +0430

    finish up game shop filters design

--- src/components/wrapper/wrapper.component.jsx
+++ src/components/wrapper/wrapper.component.jsx
@@ -6,8 +6,8 @@ const Wrapper = ({ children }) => {
   const toggleNavbar = useSelector((state) => state.toggleNavbar);
 
   return (
-    <div className={`${toggleNavbar ? '' : 'wrapper'}`}>
-      <div className={`content mb-36 mx-2 md:mx-8 lg:mx-8`}>{children}</div>
+    <div>
+      <div className={`mb-36 mx-2 md:mx-8 lg:mx-8`}>{children}</div>
     </div>
   );
 };
