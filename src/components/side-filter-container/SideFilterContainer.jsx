4edc511a79a55141ce55660d3f0c474741daacbb
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-12 14:47:24 +0430

    style/sidefilters style finish

--- src/components/side-filter-container/SideFilterContainer.jsx
+++ src/components/side-filter-container/SideFilterContainer.jsx
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 const SideFilterContainer = ({ children }) => {
+  const [showFilters, setShowFilters] = useState(false);
+
+  const toggleFilters = () => setShowFilters(!showFilters);
+
   return (
-    <div className="w-full hidden md:w-1/4 bg-darkGrey pt-6 px-6 mt-5 rounded-md h-screen">
-      {children}
-    </div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className={`${
+          showFilters ? 'inset-0' : '-inset-full'
+        } w-full transition-all duration-300 fixed md:static md:block md:w-1/4 z-20 bg-darkGrey py-20 px-6 md:mt-5 lg:mt-5 rounded-md h-screen`}
+      >
+        <div
+          onClick={toggleFilters}
+          className="absolute top-4 text-white left-4 md:hiddem lg:hidden"
+        >
+          <i className="bx bx-x text-4xl"></i>
+        </div>
+        {children}
+        <div className="text-white md:hidden lg:hidden">
+          <span className="">Show Products</span>
+        </div>
+      </motion.div>
+      <div
+        onClick={toggleFilters}
+        className="sm:hidden text-2xl text-white p-2"
+      >
+        <div className="bg-darkPurple w-1/4 p-3 rounded-md">
+          <i className="bx bx-filter-alt pr-3"></i>
+          <span>Filters</span>
+        </div>
+      </div>
+    </>
   );
 };
 
